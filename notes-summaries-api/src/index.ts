8c83f917c0c24e171605eaa297bccb6429e304ab
Ayoub Ben Chafai <aben-cha@student.1337.ma>
2025-12-28 01:28:27 +0100

    starting the server

--- notes-summaries-api/src/index.ts
+++ notes-summaries-api/src/index.ts
@@ -1,1 +1,12 @@
-const hello = 'hello';
+import express from 'express';
+import config from "./config/config";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.listen(config.port, () => {
+    console.log(`Server running on port ${config.port}`);
+});
