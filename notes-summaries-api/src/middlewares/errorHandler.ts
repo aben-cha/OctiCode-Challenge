b2da3aacb2973fde609565d953f1e185409357ec
Ayoub Ben Chafai <aben-cha@student.1337.ma>
2025-12-30 03:35:13 +0100

    update errorHandler

--- notes-summaries-api/src/middlewares/errorHandler.ts
+++ notes-summaries-api/src/middlewares/errorHandler.ts
@@ -1,20 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
-export interface AppError extends Error {
-  status?: number;
-}
-
-// export const errorHandler = (err: AppError, req: Request, res: Response, next: NextFunction) => {
-//   console.error(err);
-//   const status = err.status || 500;
-
-//   res.status(status).json({
-//     success: false,
-//     error: err.message || 'Internal Server Error',
-//   });
-// };
-
-export const errorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (error: any, req: Request, res: Response, _next: NextFunction) => {
   console.error('Error: ', error);
 
   let statusCode = 500;
